f940edd5a43d0025933100793e0d34bbcc37759c
Kavin Geofrey <kavingeofrey16@gmail.com>
2026-04-09 07:09:47 +0530

    added data cleaning notebook and initialised jupyter

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Load (Adjust paths based on your folder structure)
+train = pd.read_csv('data_set_ALL_AML_train.csv')
+test = pd.read_csv('data_set_ALL_AML_independent.csv')
+
+gene_names = train['Gene Accession Number']
+df_numeric = train.drop(['Gene Description', 'Gene Accession Number'], axis=1)
+
+
+df_numeric = df_numeric.loc[:, ~df_numeric.columns.str.contains('call')]
+
+
+X_train = df_numeric.T
+
+# Renaming columns to GAN for readability
+X_train.columns = gene_names.values
+
+print(f"New Shape: {X_train.shape}") # Should be (32, 7129)
+
+log_data = {
+    'experiment_id': [],
+    'feature_selection': [], 
+    'model_type': [],       
+    'accuracy': [],
+    'notes': []
+}
+
+# Create the dataframe and save it as your initial log
+log_df = pd.DataFrame(log_data)
+log_df.to_csv('experiment_log.csv', index=False)
